751e1cd99c4926d6392a2b799308ddfb294b4fd2
Rgonzalez717 <61915917+Rgonzalez717@users.noreply.github.com>
2020-05-05 13:28:24 +0700

    membuat main class
    memilih objek

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymophismPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows printer = new PrinterWindows() ;
+            Console.WriteLine("Pilih Printer : ");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LaserJet");
+
+            Console.Write("Pilih Nomor Printer [1/2/3] : ");
+
+            int NomorPrinter = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+
+            if(NomorPrinter == 1)
+            {
+                printer = new Epson();
+            }
+            else if(NomorPrinter == 2)
+            {
+                printer = new Canon();
+            }
+            else
+            {
+                printer = new LaserJet();
+            }
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
